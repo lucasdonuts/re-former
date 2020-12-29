dee96509646672f9c798653dc3b1442b3af177e9
Lucas <lucasdonuts@gmail.com>
2020-12-29 16:10:58 -0600

    Added edit user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def new
-    #@user = User.new
+    @user = User.new
   end
 
   def create
@@ -12,6 +12,22 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
